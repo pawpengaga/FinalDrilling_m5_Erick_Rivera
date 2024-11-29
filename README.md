e02ef8d279982d6c32ba579715f20e62a82b9de2
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-29 20:08:06 -0300

    heavy duty setup and data model completed

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Final Drilling M5 Java Trainee
 
 Un sistema de hóróscopo Chino
+
+### Usando
+
+- Posgresql
+- Java 17 (Ojala...)
+- Apache Tomcat 10.1
+
+### Paquetes
+
+- dao: Implementa las interfaces para el crud
+- interfaces: Regla las clases para ser implementadas por el DAO
+- modelo: Las entidades que tienen contacto directo con la base de datos
+- procesaconexion: La conexion con la base de datos
+- servlets: Gestionan las peticiones Http
+- filters: Las clases para restringir el acceso a ciertas vistas dependiendo del estado del usuario
+- util: Clases utilitarias generales
+
+### Flujo de accion
+ 1. Crear la logica de lectura del horoscopo
+ 2. Crear la logica de usuario después para asegurar que podré contar con los datos del horóscopo en tiempo real para la asignación durante el proceso de registro
--- data/data.sql
+++ data/data.sql
@@ -1,1 +1,33 @@
 -- Conjunto de consultas para crear todos los datos necesarios
+
+-- Crear base de datos
+CREATE DATABASE final_drilling_m5;
+
+-- Crear las tablas de horoscopo y usuarios
+CREATE TABLE horoscopo(
+  animal VARCHAR(30),
+  fecha_inicio DATE,
+  fecha_fin DATE
+);
+
+CREATE TABLE usuarios(
+  id serial NOT NULL PRIMARY KEY,
+  nombre VARCHAR(30),
+  username VARCHAR(30),
+  email VARCHAR(30),
+  fecha_nacimiento DATE,
+  password VARCHAR(30),
+  animal VARCHAR(30)
+);
+
+-- Carga de datos (Cambiar la ruta absoluta por la usada en el proyecto)
+COPY horoscopo(animal, fecha_inicio, fecha_fin)
+FROM 'C:\Users\Erick Rivera\Desktop\java-traini\MODULO 5\session 16\FinalDrilling_m5_Erick_Rivera\data\horoscopo.csv'
+DELIMITER ';'
+HEADER;
+
+INSERT INTO usuarios(nombre, username, email, fecha_nacimiento, password, animal) VALUES
+('Erick Rivera', 'admin', 'admin@admin.com', '22-03-1997', '1234', 'Buey');
+
+-- Para emergencias
+-- DROP DATABASE final_drilling_m5;
