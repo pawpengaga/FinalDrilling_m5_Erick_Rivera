b66ba81a6ded07aecb958ed066dd672fa6061a2a
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-29 20:12:55 -0300

    clase horoscopo lista

---
+++ src/main/java/com/pawpengaga/modelo/Horoscopo.java
@@ -0,0 +1,47 @@
+package com.pawpengaga.modelo;
+
+import java.time.LocalDate;
+
+/**
+ * La clase horoscopo tiene fines de solo lectura
+ */
+public class Horoscopo {
+
+  private String horoscopo;
+  private LocalDate fecha_inicio;
+  private LocalDate fecha_final;
+
+  /**
+   * @param horoscopo Un animal
+   * @param fecha_inicio Autoexplicativo
+   * @param fecha_final Autoexplicativo
+   */
+  public Horoscopo(String horoscopo, LocalDate fecha_inicio, LocalDate fecha_final) {
+    this.horoscopo = horoscopo;
+    this.fecha_inicio = fecha_inicio;
+    this.fecha_final = fecha_final;
+  }
+
+  public String getHoroscopo() {
+    return horoscopo;
+  }
+
+  public LocalDate getFecha_inicio() {
+    return fecha_inicio;
+  }
+
+  public LocalDate getFecha_final() {
+    return fecha_final;
+  }
+
+  // Metodo toString generado de manera temporal para pruebas internas
+  @Override
+  public String toString() {
+    return "{" +
+      " horoscopo='" + getHoroscopo() + "'" +
+      ", fecha_inicio='" + getFecha_inicio() + "'" +
+      ", fecha_final='" + getFecha_final() + "'" +
+      " }";
+  }
+
+}
